b3e15e65094ac898bba66e1eb4f08884db23d5e4
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-04 18:46:35 +0530

    merging main

---
+++ 0704/fristNcube.py
@@ -0,0 +1,4 @@
+n = input("Enter a number: ")
+n = int(n)
+sum = (n*(n+1)/2)**2
+print(f'The value of cube of {n} : {sum}')
---
+++ 0704/pythagorasTheorem.py
@@ -0,0 +1,7 @@
+import math
+a = input("Enter the value of base(a) : ")
+b = input("Enter the value of height(b) : ")
+a = float(a)
+b = float(b)
+c = math.sqrt(a ** 2 + b**2)
+print(f"The hypotenuse (c) is: {c}")

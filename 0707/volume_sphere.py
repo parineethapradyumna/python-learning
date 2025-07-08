bf64650f52b0a3cb034d413adc169d03d2e2e860
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-08 15:57:39 +0530

    Volume of a Sphere-calculator is added

---
+++ 0707/volume_sphere.py
@@ -0,0 +1,15 @@
+# Program to calculate the volume  of a Sphere
+#Asking the user to enter the value
+r = input("Enter the value of Radius(r): ")
+#convert the string to float
+r = float(r)
+#The formula for volume  of a Sphere
+sum = 4/3*3.14*r**3
+#print the statement
+print(f"Volume  of a Sphere is: {sum} Cm")
+
+
+
+#######OUTPUT########
+# Enter the value of Radius(r): 6
+# Volume  of a Sphere is: 904.3199999999999 Cm

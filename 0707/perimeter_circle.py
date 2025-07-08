82fd1db3b99b5be29f0bab0756672c5a44768523
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-08 14:53:14 +0530

    perimeter of the Circle - calculator added

---
+++ 0707/perimeter_circle.py
@@ -0,0 +1,10 @@
+#Creating the perimeter of the Circle Calculater
+#Asking the user to enter the value
+r = input("Enter the value of Radius(R): ")
+#Converting the string to float
+r=float(r)
+#The formula of perimeter of Circle
+sum = (2*3.14*r)
+#We have to print the value
+print(f"The perimeter of a Circle is: {sum} Cm ")
+

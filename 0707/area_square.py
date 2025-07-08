8e70a7d39ca3926971f2a455da1db1dc43e1ed00
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-08 12:05:42 +0530

    Area of square - calculating added

---
+++ 0707/area_square.py
@@ -0,0 +1,16 @@
+# We are calculating the area of Square
+#We are asking the user to give the value of side
+a = input("Enter the value of Side(S): ")
+
+#now we convert the number into the float
+a = int(a)
+
+#this is the formula for area of Square
+sum = (a**2)
+#now we will print the statement
+print(f"This is the value of area of Square: {sum} Cm")
+
+
+########OUTPUT########
+# Enter the value of Side(S): 5
+# This is the value of area of Square: 25 Cm

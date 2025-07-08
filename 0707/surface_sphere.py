11be5ee640bd7bdb4c935f25f81a8832f32e0a61
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-08 15:35:10 +0530

    Surface of a Sphere-calculator is added

---
+++ 0707/surface_sphere.py
@@ -0,0 +1,14 @@
+# Program to calculate the surface of a Sphere
+#Asking the user to enter the value
+r = input("Enter the value of Radius(r): ")
+#convert the string to float
+r = float(r)
+#the formula for surface of a Sphere
+sum = 4*3.14*r**2
+#print the statement
+print(f"Surface of a Sphere is: {sum} Cm")
+
+
+########OUTPUT##########
+# Enter the value of Radius(r): 7
+# Surface of a Sphere is: 615.44 Cm

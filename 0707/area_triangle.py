6db0d60feb21ce853b2b218310950fc4a8f94958
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-08 11:32:13 +0530

    Area Of Triangle calculator added

---
+++ 0707/area_triangle.py
@@ -0,0 +1,21 @@
+#calculate the area of a triangle
+
+#Getting Base and Height Value
+b = input("Enter the Base Value: ")
+h = input("Enter the Height Value: ")
+
+#Convert input values into integer
+b = int(b)
+h = int(h)
+
+#Formula for Area of Trinagle
+sum = (1/2 *b*h)
+
+# At last we will print the output
+print(f"This the value of Area of Triangle: {sum} Cm")
+
+###########OUTPUT##########
+# Enter the Base Value: 5
+# Enter the Height Value: 9
+# This the value of Area of Triangle: 22.5 Cm
+

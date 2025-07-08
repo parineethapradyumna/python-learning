a8dfce5f01297ee4738eccc53fd350881e4288ac
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-08 11:58:24 +0530

    Area of rectangle - calculator added

---
+++ 0707/area_rectangle.py
@@ -0,0 +1,18 @@
+# We are calculating the area of rectangle
+#We are asking the user to give the values of width and height
+w = input("Enter the value of Width(W): ")
+l = input("Enter the value of Height(H): ")
+
+#now we convert the number into the float
+w = float(w)
+l = float(l)
+
+#this is the formula for area of rectangle
+sum = w*l
+#now we will print the statement
+print(f"This is the value of area of rectangle: {sum} Cm")
+
+#########OUTPUT##########
+# Enter the value of Width(W): 5
+# Enter the value of Height(H): 8
+# This is the value of area of rectangle: 40.0 Cm

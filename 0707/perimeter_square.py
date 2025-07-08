ad2848023ab09d6e6a47d99633fb92496b59c69b
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-08 13:24:51 +0530

    Perimeter of a square-calculator is added

---
+++ 0707/perimeter_square.py
@@ -0,0 +1,14 @@
+#Creating the perimeter of the Square Calculater
+#asking the user to enter the value of a
+a = input("Enter the length of side a: ")
+#Converting the String to float
+a = float(a)
+#This is the formula for perimeter of a square
+sum= 4*a
+#Printing the output
+print(f"Perimeter of a square is: {sum} Cm")
+
+########OUTPUT########
+# Enter the length of side a: 4
+# Perimeter of a square is: 16.0 Cm
+

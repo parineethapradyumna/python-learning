2a3af2c0a6a415a37bd933296cb71f747c2fad65
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-08 11:45:14 +0530

    Area of circle calculator is added

---
+++ 0707/area_circle.py
@@ -0,0 +1,15 @@
+#Asking he user to enter the radius value
+r = input ("Enter the Radius Value: ")
+#convert the number into float because when the value is decimal we use "float" insted of "int"
+r = float(r)
+#The formula of area of circle insted of Pie ymbol we use the number value of pic
+sum = (3.14)*(r**2)
+#finaly we will print the statment
+print(f"This is the value of Area of Circle: {sum} Cm")
+
+
+
+#########OUTPUT########
+
+# Enter the Radius Value: 7
+# This s the value of Area of Circle: 153.86 Cm

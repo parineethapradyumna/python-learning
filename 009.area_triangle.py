6ed6a6aec4be52d7faaf66396f26a754b0217ee3
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 14:51:35 +0530

    area of triangle calculator added

---
+++ 009.area_triangle.py
@@ -0,0 +1,17 @@
+# We are creating the area of triangle calculator
+# We are asking the user to enter the base and height values
+b = input("Enter the value of Base(b): ")
+h = input("Enter the value of Height(h): ")
+# Converting the string to int
+b = int(b)
+h = int(h)
+# This is the formula of area of triangle
+sum = 1/2*b*h
+# We have to print the statement
+print(f"The value of area of triangle is: {sum} Cm")
+
+
+####OUTPUT####
+# Enter the value of Base(b): 3
+# Enter the value of Height(h): 9
+# The value of area of triangle is: 13.5 Cm

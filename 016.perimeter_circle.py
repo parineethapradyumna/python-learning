494b2ddad5d368cfec2de9767f45edbed0acba7c
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 15:45:46 +0530

    Perimeter of a circle calculator added

---
+++ 016.perimeter_circle.py
@@ -0,0 +1,13 @@
+# We are creating the Perimeter of a circle calculator
+# Asking the user to enter the value of radius
+r = input("Enter the value of Radius(r): ")
+# Converting the string to float
+r = float(r)
+# This is the formual of Perimeter of a circle calculator
+sum = 2*3.14*r
+# Print the output
+print(f"The value of Perimeter of a circle calculator is : {sum} Cm")
+
+####OUTPUT####
+# Enter the value of Radius(r): 3
+# The value of Perimeter of a circle calculator is : 18.84 Cm

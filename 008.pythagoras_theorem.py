46e92b1d12e6e9956c8628c42a7c10ef85c08ba9
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 14:38:58 +0530

    Pythagforas Theorem calculator added

---
+++ 008.pythagoras_theorem.py
@@ -0,0 +1,16 @@
+# We are creating the Pythagforas Theorem calculator
+# We are aksing the user to enter the value of a,b side
+a = input("Enter the value of side(a): ")
+b = input("Enter the value of side(b): ")
+# Convert the string to int
+a = int(a)
+b = int(b)
+# This is the formual of Pythagoras Theorem
+sum = a**2+b**2
+# At last we have to print the statement
+print(f"The value of Pythagoras Theorem is: {sum}")
+
+###OUTPUT###
+# Enter the value of side(a): 2
+# Enter the value of side(b): 3
+# The value of Pythagoras Theorem is: 13

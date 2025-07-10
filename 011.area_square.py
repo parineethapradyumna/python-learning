cd103f0cfce311ff6f03094ee8134f4a3505e97f
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 15:09:27 +0530

    area of square calculator added

---
+++ 011.area_square.py
@@ -0,0 +1,14 @@
+# We are creating the area of square calculator
+# We are asking the user to give the value of side
+a = input("Enter the value of Side(S): ")
+# converting the string into float
+a = float(a)
+# This is the formula for area of Square
+sum = (a**2)
+# Now we will print the statement
+print(f"This is the value of area of Square: {sum} Cm")
+
+
+####OUTPUT####
+# Enter the value of Side(S): 7
+# This is the value of area of Square: 49.0 Cm
---
+++ 012.area_circle.py
@@ -0,0 +1,9 @@
+# We are creating the area of circle calculator
+# We are asking the user to give the value of radius
+r = input("Enter the value of Radius(r): ")
+# Converting the string to float
+r = float(r)
+# This is the formual of area of circle
+sum = 3.14*r**2
+# This is the output
+print(f"The value of area of circle is: {sum} Cm")

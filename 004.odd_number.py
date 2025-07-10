efff476c52fe8ceee343bc6e2db5ef7eb94d1b82
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 13:58:59 +0530

    odd number calculator added

---
+++ 004.odd_number.py
@@ -0,0 +1,16 @@
+# we are creating even number calculator
+# we are asking the user to enter any number
+n = input("Enter a number: ")
+# converting the string to int
+n = int (n)
+# this says that if any number that is divisible by 2 but have a remainder the it is called as odd number 
+sum = (n%2!=0)
+# now we have to show the output
+print(f"The {n} is a Odd Number? : {sum} ")
+
+####OUTPUT####
+# Enter a number: 3
+# The 3 is a Odd Number? : True
+### When if it is not True then###
+# Enter a number: 8
+# The 8 is a Odd Number? : False

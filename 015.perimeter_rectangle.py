f3bc4b787f4b4ba2ef2f0744fc3add23965f5632
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 15:40:17 +0530

    perimeter of a Rectangle calculator added

---
+++ 015.perimeter_rectangle.py
@@ -0,0 +1,16 @@
+# We are creating perimeter of a Rectangle calculator
+# Asking the suer to enter the width and length 
+w = input("Enter the value of width(w): ")
+l = input("Enter the value of length(l): ")
+# Converting the string to float
+w = float(w)
+l = float(l)
+# This is the formula of perimeter of Rectangle
+sum = 2*(w+l)
+# Print the output
+print(f"This value of perimeter of rectangle is :{sum} Cm ") 
+
+####OUTPUT####
+# Enter the value of width(w): 5
+# Enter the value of length(l): 2
+# This value of perimeter of rectangle is :14.0 Cm 

bdfc1a483b0c465f4eafa2e65d8510916e65052c
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 15:25:35 +0530

    perimeter of a triangle calculator added

---
+++ 013.perimeter_triangle.py
@@ -0,0 +1,20 @@
+# We are creating perimeter of a triangle calculator 
+# Asking the user to enter the value of the sides
+a = input("Enter the value of side(a): ")
+b = input("Enter the value of side(b): ")
+c = input("Enter the value of side(c): ")
+# Converting the string to float
+a = float(a)
+b = float(b)
+c = float(c)
+# This is the formula of perimeter of a triangle
+sum = a+b+c
+# Print the output
+print(f"The value of the perimeter of a triangle is: {sum}")
+
+
+####OUTPUT####
+# Enter the value of side(a): 9
+# Enter the value of side(b): 9
+# Enter the value of side(c): 9
+# The value of the perimeter of a triangle is: 27.0

8057c65befde377d067c3ca1f49d42c767d3b587
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 16:01:58 +0530

    Area of a Octagone calculator added

---
+++ 018.area_octagone.py
@@ -0,0 +1,15 @@
+# We are creating the Area of a Octagone
+#Asking the user to enter the value
+import math
+a = input("Enter the value of side(a): ")
+#convert the string to float
+a = float(a)
+#the formula for area of a Octagone
+sum = (2*(1+math.sqrt(2))*a**2)
+#print the statement
+print(f"The value of Area of a Octagone is: {sum} Cm")
+
+
+####OUTPUT####
+# Enter the value of side(a): 7 
+# The value of Area of a Octagone is: 236.5929291125633 Cm

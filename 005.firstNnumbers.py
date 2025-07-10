40df89a207628016ea31300f88337920704ce64e
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 14:10:01 +0530

    first Natural Number Calculator added

---
+++ 005.firstNnumbers.py
@@ -0,0 +1,13 @@
+# we are creating first N Natural Number Calculator
+# asking the user to enter a number
+n = input("Enter a number: ")
+# converting the string to int
+n = int(n)
+# this is the formula of first N Natural Number 
+sum = n*(n+1)/2
+# we have to print the statement
+print(f"The value of Frist Natural Number is: {sum}")
+
+####OUTPUT####
+# Enter a number: 5
+# The value of Frist Natural Number is: 15.0

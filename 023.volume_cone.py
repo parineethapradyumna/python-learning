199277747ac30ead55729acb59ab8219fb4343d2
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-10 16:36:29 +0530

    Volume of a Cone calculator added

---
+++ 023.volume_cone.py
@@ -0,0 +1,19 @@
+# We are creating the Volume of a Cone calculator
+# Asking the user to enter the value of radius and height
+r = input("Enter the value of Radius(r): ")
+h = input("Enter the value of Height(h): ")
+#Convert the string to float
+r = float(r)
+h = float(h)
+# This is the formual of Volume of a Cone
+sum = 3.14*r**2*h/3
+# Print the output
+print(f"The value of Volume of a Cone is : {sum} Cm")
+
+
+####OUTPUT####
+# Enter the value of Radius(r): 6
+# Enter the value of Height(h): 4
+# The value of Volume of a Cone is : 150.72 Cm
+
+

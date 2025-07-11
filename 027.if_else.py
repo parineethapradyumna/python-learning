61d81457f6d49cb54f18fcbf356b6b575a72b5a8
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-11 09:59:31 +0530

    IF-ELSE Added

---
+++ 027.if_else.py
@@ -0,0 +1,24 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+if age>=18:
+    print(f"Hi {name}, Your are eligible to vote")
+else:
+    print("Your not eligible to vote")
+
+print("ThankYou")
+
+
+####OUTPUT####
+# Enter your name: Charan
+# Enter your age: 19
+# Hi Charan, Your are eligible to vote
+# ThankYou
+
+######
+
+# Enter your name: Mayukha
+# Enter your age: 16
+# Your not eligible to vote
+# ThankYou
+
+

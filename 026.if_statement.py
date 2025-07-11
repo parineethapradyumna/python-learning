c47aee94792f52e5343e332c5bb5d3379db14794
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-11 09:52:58 +0530

    If statement addded

---
+++ 026.if_statement.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+age = input("Enetr your age: ")
+age = int(age)
+if age >=18:
+    print(f"Hi {name}, Your eligible to vote")
+print("Thankyou")
+
+####OUTPUT####
+# Enter your name: Mayukha
+# Eter your age: 16
+# Thankyou
+
+# ######
+
+# Enter your name: Charan
+# Enter your age: 19
+# Hi Charan, Your eligible to vote
+# Thankyou

1effd2d24a4b1ebae5b947c80b82332875e66508
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-11 11:29:14 +0530

    Maximum of two numbers

---
+++ 0711/max_two_numbers.py
@@ -0,0 +1,18 @@
+a = int(input("Enter a number(a): "))
+b = int(input("Enter a number(b): "))
+if a > b:
+    print(f"Max = {a}")
+else:
+    print(f"Max = {b}")
+
+
+####OUTPUT####
+# Enter a number(a): 8
+# Enter a number(b): 6
+# Max = 8
+
+#####
+
+# Enter a number(a): 3
+# Enter a number(b): 9
+# Max = 9

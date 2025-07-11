4ef9ec75ed8e49ff89da9266f3d24f63d9f9033a
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-11 10:18:02 +0530

    IF STATEMENT ADDED

--- 026.if_statement.py
+++ 026.if_statement.py
@@ -5,14 +5,17 @@ if age >=18:
     print(f"Hi {name}, Your eligible to vote")
 print("Thankyou")
 
+
 ####OUTPUT####
 # Enter your name: Mayukha
-# Eter your age: 16
+# Enter your age: 12
 # Thankyou
 
 # ######
-
 # Enter your name: Charan
 # Enter your age: 19
 # Hi Charan, Your eligible to vote
 # Thankyou
+
+
+
---
+++ 028.max_of_2_numbers.py
@@ -0,0 +1,17 @@
+a = int(input("Enter a number(a): "))
+b = int(input("Enter a numnber(b): "))
+if a > b:
+    print(f"max = {a}")
+else:
+    print(f"max = {b}")
+
+####OUTPUT####
+# Enter a number(a): 9
+# Enter a numnber(b): 6
+# max = 9
+
+######
+
+# Enter a number(a): 5
+# Enter a numnber(b): 8
+# max = 8

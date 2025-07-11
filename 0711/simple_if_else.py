9d72f450d124f9558bf74d061c52362691a9beb4
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-11 11:22:57 +0530

    Simple if and else added

---
+++ 0711/simple_if_else.py
@@ -0,0 +1,21 @@
+name = input("Enter your name: ")
+age = int(input("enter your age: "))
+if age >= 18:
+    print(f"Hello {name}, You are eligible to vote")
+else:
+        print("Sorry, Your not eligible to vote")
+print("Thankyou")
+
+
+####OUTPUT####
+# Enter your name: jansi
+# enter your age: 15
+# Sorry, Your not eligible to vote
+# Thankyou
+
+##### 
+
+# Enter your name: kavya
+# enter your age: 19
+# Hello kavya, You are eligible to vote
+# Thankyou

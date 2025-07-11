8919badc36d73d8665837344ad043bb727eb7a41
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-11 10:35:27 +0530

    Minimum of 2 numbers added

---
+++ 029.min_of_2_number.py
@@ -0,0 +1,11 @@
+# We are creating the Minimum number 
+# Asking the user to enter the any two numbers in a and b 
+a = int(input("Enter a number(a): "))
+b = int(input("Enter a numnber(b): "))
+# if a is less than b
+if a < b:
+# Then the minimum number will be "a" Value
+    print(f"min = {a}")
+# Or else it it show "b" value
+else:
+    print(f"min = {b}")

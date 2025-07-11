f83710f543d3d9cadc5ad2e083538b63edc17883
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-11 11:36:55 +0530

    Minimum two numbers

---
+++ 0711/min_two_numbers.py
@@ -0,0 +1,18 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+if a < b:
+    print(f"Min: {a}")
+else:
+    print(f"Min: {b}")
+
+
+####OUTPUT####
+# Enter a number: 9
+# Enter a number: 7
+# Min: 7
+
+# #####
+
+# Enter a number: 2
+# Enter a number: 4
+# Min: 2

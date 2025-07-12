29277c60bdd5b1c484c805a169c1e7c4cd459ab8
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:29:20 +0530

    odd

---
+++ 0712/odd.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))
+sum = n%2!=0
+print(f"The {n} is a odd Number: {sum}")
+
+
+#####OUTPUT#####
+# Enter a number: 3
+# The 3 is a odd Number: True
+
+
+# Enter a number: 8
+# The 8 is a odd Number: False

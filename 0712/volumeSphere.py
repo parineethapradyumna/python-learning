925afcc768af4882d979ed6bcb727304d34feabd
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 11:28:26 +0530

    Volume of sphere

---
+++ 0712/volumeSphere.py
@@ -0,0 +1,8 @@
+r = int(input("Enter the value of radius(r): "))
+pi = 3.14
+sum= 4/3*pi*r**3
+print(f"This is the value of Volume of Sphere: {sum}")
+
+####OUTPUT#####
+# Enter the value of radius(r): 6
+# This is the value of Volume of Sphere: 904.3199999999999

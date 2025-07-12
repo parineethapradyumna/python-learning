adad3af35c685d8702d2252023272523ce40aa95
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:47:46 +0530

    triangle

---
+++ 0712/perimeterTriangle.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a value of side (A): "))
+b = int(input("Enter a value of side(B): "))
+c = int(input("Enter a value of side(c): "))
+sum = a+b+c
+print(f"This is te value of perimeter of triangle: {sum}")
+
+####OUTPUT#####
+# Enter a value of side (A): 4
+# Enter a value of side(B): 4
+# Enter a value of side(c): 4
+# This is te value of perimeter of triangle: 12

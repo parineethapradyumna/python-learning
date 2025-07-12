7e65846346ff8955f8b01652b2a5a8f85bbf8b91
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:36:19 +0530

    area of triangle

---
+++ 0712/areaTriangle.py
@@ -0,0 +1,9 @@
+b = int(input("Enter the value of base(b): "))
+h = int(input("Enter the value of height(h): "))
+sum = 1/2*b*h
+print(f"This is the value of area of triangle:{sum} ")
+
+#####OUT####
+# Enter the value of base(b): 5
+# Enter the value of height(h): 8
+# This is the value of area of triangle:20.0 

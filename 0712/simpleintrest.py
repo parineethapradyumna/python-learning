46f1139bdcf68661c01ee649dadcc9d9e000235c
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 09:33:47 +0530

    simple intrest added

---
+++ 0712/simpleintrest.py
@@ -0,0 +1,11 @@
+p = int(input("Enter the value principal amount: "))
+r = int(input("Enter the value of rate of intrest: "))
+t = int(input("Enter the value of time: "))
+sum = p*r*t/100
+print(f"This is the value of simple intrest:{sum} ")
+
+####OUTPUT####
+# Enter the value principal amount: 9000
+# Enter the value of rate of intrest: 4
+# Enter the value of time: 2
+# This is the value of simple intrest:720.0

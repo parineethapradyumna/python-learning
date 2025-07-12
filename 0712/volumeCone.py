32706a2ceaabb7c9c9affe8bba1038d346088592
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 11:23:15 +0530

    Volume of cone

---
+++ 0712/volumeCone.py
@@ -0,0 +1,10 @@
+r = float(input("Enter the value of radius(r): "))
+h = float(input("Enter the value of height(h): "))
+pi = 3.14
+sum = pi*r**3*h/3
+print(f"This is the value of Volume of cone: {sum}")
+
+#####OUTPUT######
+# Enter the value of radius(r): 6
+# Enter the value of height(h): 9
+# This is the value of Volume of cone: 2034.72

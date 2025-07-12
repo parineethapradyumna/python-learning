09a785849a91625b025eb2753afa3039dc4a5d44
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 11:00:23 +0530

    AP

---
+++ 0712/arthimaticprogression.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the value of first term: "))
+n = int(input("Enter the value of position: "))
+d = int(input("Enter the value of common dffrence: "))
+sum = a*(n-1)*d
+print(f"This is the vaklue of AP: {sum}")
+
+
+#####OUTPUT######
+# Enter the value of first term: 5
+# Enter the value of position: 8
+# Enter the value of common dffrence: 9
+# This is the vaklue of AP: 315

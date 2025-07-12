aee17fbb54cbce930aa936489b8965866fde0db4
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 11:19:46 +0530

    surface of cylinder

---
+++ 0712/surfaceCylinder.py
@@ -0,0 +1,10 @@
+r = int(input("Enter the value of radius(r): "))
+h = int(input("Enter the value of height(h): "))
+pi = 3.14
+sum = 2*pi*r**2+ pi*r*h
+print(f"This is the value of surface of cylinder: {sum}")
+
+#####OUTPUT######
+# Enter the value of radius(r): 5
+# Enter the value of height(h): 10
+# This is the value of surface of cylinder: 314.0

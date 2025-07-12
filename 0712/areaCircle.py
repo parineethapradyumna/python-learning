ef7bff8a496da9d54ab7bad901bb8652b20eab36
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:45:04 +0530

    Circle

---
+++ 0712/areaCircle.py
@@ -0,0 +1,8 @@
+r = int(input("Enter a number: "))
+pi = 3.14
+sum = pi*r**2
+print(f"This is the value of area of circle: {sum}")
+
+#####OUTPUT###
+# Enter a number: 5
+# This is the value of area of circle: 78.5

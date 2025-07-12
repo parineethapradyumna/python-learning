988d9020e57bef0a6197bd146522cd02559a4fb5
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 12:13:11 +0530

    Commit

---
+++ 0712/min_4.py
@@ -0,0 +1,28 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+d = int(input("Enter a number: "))
+
+if a<b:
+    if a<c:
+        if a<d:
+            print(f"Min = {a}")
+        else:
+            print(f"Min = {d}")
+    
+    else:
+        if d<c:
+            print(f"Min = {d}")
+        else:
+            print(f"Min = {c}")
+else:
+    if b < c:
+        if b < d:
+            print(f"Min: {b}")
+        else:
+            print(f"Min: {d}")
+    else:
+        if c < d:
+            print(f"Min: {c}")
+        else:
+            print(f"Min: {d}")

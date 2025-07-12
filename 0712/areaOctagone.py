4564e8e154567cc10146d72f6cac777756506996
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 11:04:54 +0530

    octagone

---
+++ 0712/areaOctagone.py
@@ -0,0 +1,9 @@
+import math
+a = int(input("Enter a value of side(a): "))
+sum = 2*(1+math.sqrt(2))*a**2
+print(f"This is the value of area of octagone: {sum} Cm")
+
+
+###OUTPUT#####
+# Enter a value of side(a): 4
+# This is the value of area of octagone: 77.25483399593904 Cm

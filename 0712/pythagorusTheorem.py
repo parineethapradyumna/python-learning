72fab53db0b19519e11caf135c4e53751daa1e99
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:33:18 +0530

    PT added

---
+++ 0712/pythagorusTheorem.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number(a): "))
+b = int(input("Enter a number(b): "))
+
+sum = a**2 + b**2
+print(f"The value of pythagorus theorem is: {sum}")
+
+
+####OUTPUT###
+# Enter a number(a): 5
+# Enter a number(b): 6
+# The value of pythagorus theorem is: 61

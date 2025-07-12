44e4a3fbcf44c1298ad3840a7a6e557ae777005b
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:50:52 +0530

    perimeter of reactangle

---
+++ 0712/perimeterReactangle.py
@@ -0,0 +1,9 @@
+w = int(input("Enter a value of width(w): "))
+l = int(input("Enter a value of length(l): "))
+sum = 2*(w+l)
+print(f"This is the value of perimeter of reactangle: {sum}")
+
+###OUTPUT#####
+# Enter a value of width(w): 6
+# Enter a value of length(l): 5
+# This is the value of perimeter of reactangle: 22

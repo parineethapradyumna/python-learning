dec93795c576a3e7a756737d5ac0f93887914606
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:39:48 +0530

    Area of Reactangle

---
+++ 0712/areaReactangle.py
@@ -0,0 +1,10 @@
+w = int(input("Enter the value of Width(W): "))
+l = int(input("Enter the value of Length(L): "))
+sum = (w*l)
+print(f"this is value of area of rectangle:{sum} ")
+
+
+##Output###
+# Enter the value of Width(W): 3
+# Enter the value of Length(L): 4
+# this is value of area of rectangle:12 

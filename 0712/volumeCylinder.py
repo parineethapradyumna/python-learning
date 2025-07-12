e36a7bd11a9e58e93e6036882c388c845c4f8e5c
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 11:25:38 +0530

    Volume of cylinder

---
+++ 0712/volumeCylinder.py
@@ -0,0 +1,10 @@
+r = float(input("Enter the value of radius(r): "))
+h = float(input("Enter the value of height(h): "))
+pi = 3.14
+sum = pi*r**2*h
+print(f"This is the value of volume of cylinder: {sum}")
+
+####OUTPUT####
+# Enter the value of radius(r): 4
+# Enter the value of height(h): 10
+# This is the value of volume of cylinder: 502.40000000000003

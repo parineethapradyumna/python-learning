23a0672515bb3079dfc5812e1aa7e2d86590c627
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:20:37 +0530

    squares

---
+++ 0712/FirstNnumbers.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a Number: "))
+sum= n*(n+1)/2
+print(f"This is the value of sum of first N nautral numbers: {sum} ")
+
+####OUTPUT####
+# Enter a Number: 3
+# This is the value of sum of first N nautral numbers: 6.0 
---
+++ 0712/fristNsquare.py
@@ -0,0 +1,7 @@
+n = int(input("enter a number: "))
+sum= n*(n+1)*(2*n+1)/6
+print(f"this is the value of sum of square of first n natural numbers: {sum}")
+
+####OUTPUT#####
+# enter a number: 3
+# this is the value of sum of square of first n natural numbers: 14.0

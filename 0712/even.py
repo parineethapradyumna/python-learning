c58eb35712bec545a663c62f943ed1cc68ab7746
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:27:03 +0530

    even

---
+++ 0712/even.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+sum = n%2==0
+print(f"The {n} is a even number ? : {sum} ")
+
+
+
+####OUTPUT###
+
+# Enter a number: 3
+# The 3 is a even number ? : False 
+
+#####
+
+# Enter a number: 2
+# The 2 is a even number ? : True 

6ec57d854d4983d15f98b495ece41a76f1e045ec
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:23:53 +0530

    Cube

---
+++ 0712/fristNcube.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+sum = (n*(n+1)/2)**2
+print(f"This is the value of sum of cube of first natural cube: {sum}")
+
+
+####OUTPUT####
+# Enter a number: 2
+# This is the value of sum of cube of first natural cube: 9.0

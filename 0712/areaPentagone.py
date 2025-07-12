75973b9f14198de36d766571b8f1ec215292a52a
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 11:09:07 +0530

    Area of pentagone

---
+++ 0712/areaPentagone.py
@@ -0,0 +1,9 @@
+import math
+a = float(input("Enter a value of side(a): "))
+sum = 1/4*(math.sqrt(5)*5+2*math.sqrt(5))*a**2
+print(f"This is the value of Area of pentagone: {sum} Cm")
+
+
+####OUTPUT####
+# Enter a value of side(a): 4
+# This is the value of Area of pentagone: 62.609903369994115 Cm

feef27a05bb4c5ee5200d59de0af8e385d62e6bd
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 11:16:16 +0530

    Surface of Sphere

---
+++ 0712/surfaceSphere.py
@@ -0,0 +1,9 @@
+r = int(input("Enter the value of radius: "))
+pi = 3.14
+sum = 4*pi*r**2
+print(f"This is the value of surface of sphere: {sum} Cm")
+
+
+# ##OUTPUT#####
+# Enter the value of radius: 2
+# This is the value of surface of sphere: 50.24 Cm

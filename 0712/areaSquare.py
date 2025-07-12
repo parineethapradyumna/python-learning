ee54eaf6bcda0da1bbbc840f28ab274af9f5237a
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:42:32 +0530

    Square

---
+++ 0712/areaSquare.py
@@ -0,0 +1,7 @@
+a = int(input("Enter of number: "))
+sum = a**2
+print(f"This is the value of area of square: {sum}")
+
+######OUTPUT######
+# Enter of number: 7
+# This is the value of area of square: 49

11e18ee535b94ab8de7087ba5ec3c41928d1183c
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-12 10:54:21 +0530

    perimeter of square

---
+++ 0712/perimetrSquare.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the value of side(a): "))
+sum = 4*a
+print(f"This is the value of perimeter of a square: {sum}")
+
+
+####OUTPUT####
+# Enter the value of side(a): 8
+# This is the value of perimeter of a square: 32

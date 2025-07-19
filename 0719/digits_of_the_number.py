26cb2f9adb0a17101451e339f435bb905f86b9d7
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-19 14:43:11 +0530

    First Commit

---
+++ 0719/digits_of_the_number.py
@@ -0,0 +1,28 @@
+# For Loop
+n = int(input("Enter a number: "))
+i = n
+while i>0:
+    rem = i%10
+    print(rem)
+    i = i//10
+
+
+# While Loop
+n = int(input("Enter a number: "))
+i =n
+length = len(str(n))
+for i in range(length):
+    rem = n%10
+    print(rem)
+    n = n//10
+
+
+
+###Output###
+# Enter a number: 456789
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4

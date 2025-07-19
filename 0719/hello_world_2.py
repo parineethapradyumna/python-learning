090874e183919572e67558e2318a5d66e30168ab
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-19 11:50:50 +0530

    First Commit

--- 0719/hello_world_2.py
+++ 0719/hello_world_2.py
@@ -1,2 +1,11 @@
 for i in range(7):
     print(i,"Hello World")
+
+###OUTPUT#####
+# 0 Hello World
+# 1 Hello World
+# 2 Hello World
+# 3 Hello World
+# 4 Hello World
+# 5 Hello World
+# 6 Hello World

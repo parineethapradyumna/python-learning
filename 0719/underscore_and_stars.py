b6cee8110572a9ddecae173694c56dfb6d416091
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-21 17:09:38 +0530

    First Commit

---
+++ 0719/underscore_and_stars.py
@@ -0,0 +1,25 @@
+rows =5
+row = 1
+while(row<= rows):
+
+    u_col= 1
+    while(u_col <= rows-row):
+        print("-",end=" ")
+        u_col = u_col+1
+
+
+    s_col =1
+    while (s_col <= row):
+        print("* " , end=" ")
+        s_col = s_col+1
+
+    print()
+    row = row+1
+
+
+####OUTPUT#####
+# - - - - *  
+# - - - *  *  
+# - - *  *  *  
+# - *  *  *  *  
+# *  *  *  *  *  

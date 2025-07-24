0d720ec1106163f038006381d1d57c5f0fdc31f8
Parineetha Pradyumna Vanarasa <parineetha.vanarasa@gmail.com>
2025-07-24 12:14:01 +0530

    First Commit

--- 0724/max_of_list.py
+++ 0724/max_of_list.py
@@ -3,5 +3,10 @@ max = nums[0]
 for i in range(len(nums)):
     if(nums[i]>max):
         max = nums[i]
-
 print(f"Max = ",max)
+
+# a = int(input("Enter a number(1): "))
+# b = int(input("Enter a number(2): "))
+# c = int(input("Enter a number(3): "))
+# d = int(input("Enter a number(4): "))
+# e = int(input("Enter a number(5): "))
---
+++ 0724/swapping_two_numbers.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number(a): "))
+b = int(input("Enter a number(b): "))
+print(f"Before swapping: a = {a}, b = {b}")
+print(f"After swapping : a = {b}, b = {a}")
